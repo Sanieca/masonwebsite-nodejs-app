52b0ac256a69a20d6a9169c10369521580b851fe
Sanieca <masonsanieca7@gmail.com>
2024-09-07 14:24:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# masonwebsite-nodejs-app
+ 
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/gallery', (req, res) => res.render('about'));
+app.get('/services', (req, res) => res.render('services'));
+app.get('/contact', (req, res) => res.render('contact'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
